4bab87da64bab21171761319ef9c89c65795ef08
Gilad Hoze <gilad2210@gmail.com>
2021-07-24 17:36:54 +0300

    Changed API key

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const orgType = "all";
-export const apiToken = "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"
+export const apiToken = "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"
 export const secret = "EVENTS_CHANGES"
 export const serverPort = 8000;
