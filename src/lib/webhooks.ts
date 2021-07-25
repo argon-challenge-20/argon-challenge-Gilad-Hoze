3fb73ebe5b51478c0ecc56232248280e84aa5486
Gilad Hoze <gilad2210@gmail.com>
2021-07-25 08:26:16 +0300

    Added logs

--- src/lib/webhooks.ts
+++ src/lib/webhooks.ts
@@ -5,13 +5,14 @@ const reposManager = require('./repos-manager.service');
 export var webhookHandler = GithubWebHook({ path: '/hook', secret: secret });
 
 // Handle the publicized or privatized event and if the event is on a protected repo
-// than it keep the repo's visibility the same.
+// than it keeps the repo's visibility the same.
 webhookHandler.on('*', function (event, repo, data) {
   if (data.action == "privatized" || data.action == "publicized") {
     console.log(repo + " was " + data.action);
     const repoIndex = reposManager.orgRepos.findIndex(orgRepo => orgRepo.name === repo);
     if (reposManager.orgRepos[repoIndex].protected) {
       reposManager.setVisibility(repo, reposManager.orgRepos[repoIndex].private)
+      console.log("Protected " + repo)
     } else {
       reposManager.orgRepos[repoIndex].private = data.action == "privatized" ? true : false;
     }
